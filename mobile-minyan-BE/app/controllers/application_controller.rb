1ffb06863041302c0a2298b1ebe381a25a6ecb8e
ryan horowitz <ryan.w.horowitz@gmail.com>
2023-11-08 13:22:23 -0500

    added migrations to users model. name changed to username. admin field added with boolean value. rails console shows users have been add to db.

--- mobile-minyan-BE/app/controllers/application_controller.rb
+++ mobile-minyan-BE/app/controllers/application_controller.rb
@@ -1,2 +1,21 @@
-class ApplicationController < ActionController::Base
+class ApplicationController < ActionController::API
+  include ActionController::Cookies
+  before_action :authorized
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+
+  def authorized
+    return render json: { error: 'Not authorized' }, status: :unauthorized unless session.include? :user_id
+  end
+
+  def render_unprocessable_entity_response(invalid)
+    render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
+  end
+
+  private
+
+  def current_user
+    return nil unless session[:user_id]
+
+    user = User.find(session[:user_id])
+  end
 end
