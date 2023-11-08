71587c80d5a7587764946f77077d225c59f19986
ryan horowitz <ryan.w.horowitz@gmail.com>
2023-11-08 13:00:13 -0500

    added rack/cors gem

--- mobile-minyan-BE/config/routes.rb
+++ mobile-minyan-BE/config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :users, only: %i[create destroy update]
+  resources :shuls, only: %i[index show]
+  resources :user_service, only: %i[create destroy]
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get '/auth', to: 'users#show'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  post '/contact-shul', to: 'contact_shul#send_email'
+  # Routing logic: fallback requests for React Router.
+  # Leave this here to help deploy your app later!
+  get '*path', to: 'fallback#index', constraints: ->(req) { !req.xhr? && req.format.html? }
 end
