db41a9dc0b016f6b616fad2065f85c46bf8f41b4
ryan horowitz <ryan.w.horowitz@gmail.com>
2023-11-08 16:30:01 -0500

    restructured architecture. all active files are living in app/

--- App.js
+++ App.js
@@ -1,22 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-// import SplashScreen from 'react-native-splash-screen';
+// import 'react-native-gesture-handler'
+// import { StatusBar } from 'expo-status-bar';
+// import { StyleSheet, Text, View } from 'react-native';
+// // import SplashScreen from 'react-native-splash-screen';
 
-export default function App() {
+// export default function App() {
 
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+//   return (
+//     <View style={styles.container}>
+//       <Text>Open up App.js to start working on your app!!</Text>
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
---
+++ app/index.js
@@ -0,0 +1,25 @@
+import 'react-native-gesture-handler'
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import Login from './login';
+// import SplashScreen from 'react-native-splash-screen';
+
+export default function App() {
+
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!!</Text>
+      <StatusBar style="auto" />
+      <Login />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
