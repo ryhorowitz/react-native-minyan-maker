be6ebaa6caba89caf3b412128fb594ed2f7d9707
ryan horowitz <ryan.w.horowitz@gmail.com>
2023-11-08 14:06:53 -0500

    added   "proxy": "http://localhost:3000" to package.json

--- app/login.js
+++ app/login.js
@@ -12,6 +12,7 @@ export default function Login() {
     } else {
       alert('Login failed');
     }
+    fetch(`localhost`)
   };
 
   return (
--- mobile-minyan-BE/app/controllers/sessions_controller.rb
+++ mobile-minyan-BE/app/controllers/sessions_controller.rb
@@ -2,6 +2,7 @@ class SessionsController < ApplicationController
   skip_before_action :authorized, only: :create
 
   def create
+    # puts "I hit the sessions controller"
     user = User.find_by(username: params[:username])
     if user&.authenticate(params[:password])
       session[:user_id] = user.id
