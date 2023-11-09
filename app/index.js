415632136fa1f13ebfd10f58be59da12083366d9
ryan horowitz <ryan.w.horowitz@gmail.com>
2023-11-09 15:44:11 -0500

    keep getting this nested NavContainer error, not sure why

--- app/index.js
+++ app/index.js
@@ -1,17 +1,24 @@
 import 'react-native-gesture-handler'
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
 import Login from './login';
 // import SplashScreen from 'react-native-splash-screen';
+
+const Stack = createNativeStackNavigator()
 
 export default function App() {
 
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!!</Text>
-      <StatusBar style="auto" />
-      <Login />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={Login} />
+        {/* <Stack. */}
+      </Stack.Navigator>
+    </NavigationContainer>
+
   );
 }
 
